fb9b2420133d639f1accf0536f1e46e24bb67842
Marcos Paulo <marcosazevedo2112@gmail.com>
2022-11-14 22:27:54 -0300

    rotas de atendimento e termino prontas

---
+++ README.md
@@ -0,0 +1,8 @@
+# Microserviço: Sistema de pedidos
+Esse repositorio apresenta o codigo fonte do microserviço ````ms-pedidos````, esse microserviço é utilizado de forma fundamental pelo QG, assim sendo de extrema importancia seu pleno funcionamento.
+
+### Instalação e praparação para rodar
+#### Requisitos
+- Ter em total funcionamento o microserviço de autenticação, o tutorial de instalação e execução do mesmo pode ser encontrado nesse link
+
+
--- src/controller/pedido.js
+++ src/controller/pedido.js
@@ -2,26 +2,37 @@ const { Router } = require("express");
 const dbHelper = require("framework");
 const Pedido = require("../model/pedido");
 const auth = require("../middlewares/auth");
+const validator = require("validator");
 
 const router = Router();
 
 // auth é um middleware que verifica se o usuário está logado
-router.post((req, res, next) => {
-  auth(req, res, next);
+router.use((req, res, next) => {
+  if (req.method == "POST" || req.originalUrl == "/pedidos/active") {
+    auth(req, res, next);
+  } else {
+    next();
+  }
 });
 
-router.get("/:id", (req, res) => {
+// Pega informações do pedido com id=id
+router.get("/:id", (req, res, next) => {
   const { id } = req.params;
-  dbHelper
-    .selectWhere("pedido", `id=${id}`)
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(500).send("Internal Server Error");
-    });
+  if (isNaN(id)) {
+    next();
+  } else {
+    dbHelper
+      .selectWhere("pedido", `id=${id}`)
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).send("Internal Server Error");
+      });
+  }
 });
 
+// Insere um pedido
 router.post("/", (req, res) => {
   const {
     token,
@@ -42,7 +53,7 @@ router.post("/", (req, res) => {
     campus
   ) {
     Pedido.createPedido(
-      "1",
+      req.body.user.id,
       name,
       category_id,
       building_id,
@@ -51,35 +62,54 @@ router.post("/", (req, res) => {
       campus
     )
       .then((result) => {
+        console.log(result);
         if (result != false) {
-            let time = new Date();
-          dbHelper.insertInto(
-            "pedido",
-            [
-              "requester_id",
-              "supplier_id",
-              "state",
-              "name",
-              "category_id",
-              "building_id",
-              "description",
-              "localization",
-              "created_at",
-              "updated_at",
-              "campus",
-            ],
-            [result.requester_id, null, 'Aberto', result.name, result.category_id, result.building_id, result.description, result.localization, time.toISOString(), time.toISOString(), result.campus_id]
-          ).then((result) => {
-            res.status(200).json(result);
-          }).catch((err) => {
-            res.status(500).send("Internal Server Error");
-          });
+          console.log(result);
+          let time = new Date();
+          console.log("id:" + result.requester_id);
+          dbHelper
+            .insertInto(
+              "pedido",
+              [
+                "requester_id",
+                "supplier_id",
+                "state",
+                "name",
+                "category_id",
+                "building_id",
+                "description",
+                "localization",
+                "created_at",
+                "updated_at",
+                "campus",
+              ],
+              [
+                result.requester_id,
+                null,
+                "Aberto",
+                result.name,
+                result.category_id,
+                result.building_id,
+                result.description,
+                result.localization,
+                time.toISOString(),
+                time.toISOString(),
+                result.campus_id,
+              ]
+            )
+            .then((result) => {
+              res.status(200).json(result);
+            })
+            .catch((err) => {
+              res.status(500).send("Internal Server Error");
+            });
         } else {
           //bad request
           res.status(400).send("Bad Request");
         }
       })
       .catch((err) => {
+        console.log(err);
         res.status(500).send("Internal Server Error");
       });
   } else {
@@ -87,6 +117,7 @@ router.post("/", (req, res) => {
   }
 });
 
+// Pega todos os pedidos
 router.get("/", (req, res) => {
   dbHelper
     .getAllLines("pedido")
@@ -96,6 +127,99 @@ router.get("/", (req, res) => {
     .catch((err) => {
       res.status(500).send("Internal Server Error");
     });
+});
+
+// Pega todos os pedidos ativos de um usuario identificado por token
+router.get("/active", (req, res) => {
+  const { user } = req.body;
+  if (user) {
+    dbHelper
+      .selectWhere(
+        "pedido",
+        `(requester_id=${user.id} OR supplier_id=${user.id}) AND (state='Aberto' OR state='Em Andamento')`
+      )
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).send("Internal Server Error");
+      });
+  }
+});
+
+// Atende um pedido com id=id e token de usuario
+router.post("/attend/:id", (req, res) => {
+  const pedidoId = req.params.id;
+  const { user } = req.body;
+  if (user && pedidoId) {
+    if(validator.isInt(pedidoId) && pedidoId > 0){
+      dbHelper
+      .selectWhere("pedido", `id=${pedidoId}`)
+      .then((pedido) => {
+        if (pedido[0] && pedido[0].state == "Aberto") {
+          if (pedido[0].requester_id != user.id) {
+            let date = new Date();
+            dbHelper
+              .customQuery(
+                `UPDATE pedido SET supplier_id=${user.id}, state='Em Andamento', updated_at='${date.toISOString()}' where id=${pedidoId}`
+              )
+              .then((result) => {
+                res.status(200).json(result);
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).send("Internal Server Error");
+              });
+          } else {
+            res.status(409).send("Conflict");
+          }
+        } else {
+          res.status(409).send("Pedido não existe ou já foi atendido");
+        }
+      })
+      .catch((err) => {
+        res.status(500).send("Internal Server Error");
+      });
+    }else{
+      res.status(400).send("Bad Request");
+    }
+  } else {
+    res.status(400).json("Bad Request");
+  }
+});
+
+// Finaliza um pedido com id=id e token de usuario
+router.post("/finish/:id", (req, res) => {
+  const pedidoId = req.params.id;
+  const {user} = req.body;
+  const {rating} = req.body;
+  if (user && pedidoId && rating) {
+    if(pedidoId>0 && validator.isInt(pedidoId) && validator.isInt(rating) && rating>=-1 && rating<=1){
+      dbHelper.selectWhere("pedido", `id=${pedidoId}`).then((pedido) => {
+        pedido = pedido[0];
+        if (pedido && pedido.supplier_id==user.id && pedido.state == "Em Andamento") {
+          // Pedido atende a todos requisitos para ser fechado
+          let date = new Date();
+          dbHelper.customQuery(`UPDATE pedido SET state='Fechado', updated_at='${date.toISOString()}' where id=${pedidoId}`).then((result) => {
+            res.status(204).json();
+            //TODO: Atualizar rating do usuario que fez o pedido.
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+          });
+        }else{
+          res.status(409).send("Pedido não existe ou já foi finalizado");
+        }
+      }).catch((err) => {
+        console.log(err);
+        });
+    }else{
+      res.status(400).send("Bad Request");
+    }
+  }else{
+    res.status(400).send("Bad Request");
+  }
 });
 
 module.exports = router;
